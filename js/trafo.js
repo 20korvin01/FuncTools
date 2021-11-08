84ae084d24e46b08d8445951b1bc05fe7edcbc56
Korvin Venzke <korvin.venzke@gmail.com>
2021-11-08 13:23:12 +0100

    Korvin
    ....

--- js/trafo.js
+++ js/trafo.js
@@ -1,26 +1,25 @@
 function himmelTrafo() {
-    var x = document.getElementById("grad__in").value;
+    var grad = document.getElementById("grad__in").value;
 
-    if (isNaN(x) === true) {
-        window.alert('\n\n Eingebefehler!\n -> Eingabe muss eine Zahl sein!')
+    if (isNaN(grad) === true) {
+        window.alert('\n\n Eingebefehler!\n -> Eingabe muss eine Zahl sein!');
     }
-    else if (Number(x) < 0 || Number(x) > 360 ) {
-        window.alert('\n\n Eingebefehler!\n -> Zahl muss zwischen 0 und 360 liegen!')
+    else if (Number(grad) < 0 || Number(grad) > 360 ) {
+        window.alert('\n\n Eingebefehler!\n -> Zahl muss zwischen 0 und 360 liegen!');
     }
     else {
-        var anteil = 360 / 16
-        var ende = 360 / 32
-        
-        var j = 0
+        var wind = ['Norden', 'Nord-Nord-Osten', 'Nord-Osten', 'Ost-Nord-Osten', 'Osten', 'Ost-Süd-Osten', 'Süd-Osten', 'Süd-Süd-Osten', 'Süden', 'Süd-Süd-Westen', 'Süd-Westen', 'West-Süd-Westen', 'Westen', 'West-Nord-Westen', 'Nord-West', 'Nord-Nord-Westen']
+        var anteil = 360 / 16;
+        var ende = 360 / 32;
+        var j = 0;
         
         while (grad > ende) {
-            ende = ende + anteil
-            j += 1
+            ende += anteil;
+            j++;
         }
-            
         if (j === 16) {
-            j = 0
+            j = 0;
         }
-        window.alert(j)
+        window.alert(wind[j]);
     }
 }
