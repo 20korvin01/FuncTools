dee6de3efd3b4205e053175afdfef3e1844ed616
Korvin Venzke <korvin.venzke@gmail.com>
2021-11-09 14:54:21 +0100

    Korvin
    WINDROSE!!

--- js/trafo.js
+++ js/trafo.js
@@ -2,13 +2,17 @@ function himmelTrafo() {
     var grad = document.getElementById("grad__in").value;
 
     if (isNaN(grad) === true) {
-        window.alert('\n\n Eingebefehler!\n -> Eingabe muss eine Zahl sein!');
+        document.getElementById("grad__out").value = 'Eingebefehler! \n Eingabe muss eine Zahl sein!'; // input should be a number
+        document.getElementById("windrose").src = '../img/windrosen/windrose_raw.png'
     }
-    else if (Number(grad) < 0 || Number(grad) > 360 ) {
-        window.alert('\n\n Eingebefehler!\n -> Zahl muss zwischen 0 und 360 liegen!');
+    else if (Number(grad) < 0) {
+        document.getElementById("grad__out").value = 'Eingebefehler! \n Zahl muss positiv sein!'; // input should be positive
+        document.getElementById("windrose").src = '../img/windrosen/windrose_raw.png'
     }
     else {
-        var wind = ['Norden', 'Nord-Nord-Osten', 'Nord-Osten', 'Ost-Nord-Osten', 'Osten', 'Ost-Süd-Osten', 'Süd-Osten', 'Süd-Süd-Osten', 'Süden', 'Süd-Süd-Westen', 'Süd-Westen', 'West-Süd-Westen', 'Westen', 'West-Nord-Westen', 'Nord-West', 'Nord-Nord-Westen']
+        var wind = ['Norden', 'Nord-Nord-Osten', 'Nord-Osten', 'Ost-Nord-Osten', 'Osten', 'Ost-Süd-Osten', 'Süd-Osten', 'Süd-Süd-Osten', 'Süden', 'Süd-Süd-Westen', 'Süd-Westen', 'West-Süd-Westen', 'Westen', 'West-Nord-Westen', 'Nord-Westen', 'Nord-Nord-Westen']
+        
+        var grad = grad % 360   // effective number to valuate
         var anteil = 360 / 16;
         var ende = 360 / 32;
         var j = 0;
@@ -20,6 +24,7 @@ function himmelTrafo() {
         if (j === 16) {
             j = 0;
         }
-        window.alert(wind[j]);
+        document.getElementById("grad__out").value = wind[j];
+        document.getElementById("windrose").src = `../img/windrosen/${wind[j].toLowerCase()}.png`
     }
 }
