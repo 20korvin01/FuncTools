a0312c3a34d616cba998648e80d6ec218a3aab06
Korvin Venzke <korvin.venzke@gmail.com>
2021-11-11 19:01:42 +0100

    Korvin
    Welcome Leaflet!

---
+++ js/function.js
@@ -0,0 +1,26 @@
+var osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a>',
+    osm = L.tileLayer(osmUrl, {
+        maxZoom: 18,
+        attribution: osmAttrib
+    });
+
+var map = L.map('map').setView([53.540309, 10.004968], 12).addLayer(osm);
+
+function onMapClick(e) {
+    var marker = L.marker(e.latlng, {
+        draggable: true,
+        title: "Resource location",
+        alt: "Resource Location",
+        riseOnHover: true
+    }).addTo(map)
+        .bindPopup(e.latlng.toString()).openPopup();
+
+    marker.on("dragend", function (ev) {
+
+        var chagedPos = ev.target.getLatLng();
+        this.bindPopup(chagedPos.toString()).openPopup();
+    });
+}
+
+map.on('click', onMapClick);
